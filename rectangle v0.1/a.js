39d2b77d3467bbace18c217ae7a4ac84e0a6bfeb
Liwenroul <2728608191@qq.com>
2020-03-31 16:24:45 +0800

    cv1

--- rectangle v0.1/a.js
+++ rectangle v0.1/a.js
@@ -16,10 +16,16 @@ $(function(){
         var h=Number($height.val());
 
     //判断小数点后的有效位数
+    if($width.val().indexOf(".")!=-1){
         var wlength=$width.val().split(".")[1].length;
         var hlength=$height.val().split(".")[1].length;
         var n1=wlength<hlength?hlength:wlength;
         var n2=wlength+hlength;
+    }
+    else{
+        n1=1;
+        n2=1;
+    }
     //calculate
         var p=2*roundFractional(w+h,n1);
         var a=roundFractional(w*h,n2);
