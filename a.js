921acedcb19397dcec99a0fe644564615a2257e1
Liwenroul <2728608191@qq.com>
2020-04-09 14:48:33 +0800

    v0.3

--- a.js
+++ a.js
@@ -1,37 +1,18 @@
-$(function(){
-    //get dom elem
-    var $width=$("#width");
-    var $height=$("#height");
-    var $btn=$("#calculate");
-    var $perimeter=$("#perimeter");
-    var $area=$("#area");
-    //浮点误差
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-    }
-    /**calc button click event */
-    $btn.click(function(){
-    //get value
-        var w=Number($width.val());
-        var h=Number($height.val());
-
-    //判断小数点后的有效位数
-    if($width.val().indexOf(".")!=-1){
-        var wlength=$width.val().split(".")[1].length;
-        var hlength=$height.val().split(".")[1].length;
-        var n1=wlength<hlength?hlength:wlength;
-        var n2=wlength+hlength;
-    }
-    else{
-        n1=1;
-        n2=1;
-    }
-    //calculate
-        var p=2*roundFractional(w+h,n1);
-        var a=roundFractional(w*h,n2);
-    //output
-        $perimeter.val(p);
-        $area.val(a);
-    })
-    
-});
+/* global Rectangle: true */
+$(function() {
+    var $width = $('#width'),
+        $height = $('#height'),
+        $btnCal = $('#calculate'),
+        $perimeter = $('#perimeter'),
+        $area = $('#area');
+  
+    $btnCal.click(function(){
+      var w = $width.val(),
+          h = $height.val();
+  
+      var r = new Rectangle(w, h);
+  
+      $perimeter.val(r.perimeter());
+      $area.val(r.area());
+    });
+  });
